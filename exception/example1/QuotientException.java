c2906daba59b52d86276fa2e698e5e9d2461a969
Angelica Bernard <48478448+acbernard1@users.noreply.github.com>
2019-03-12 10:36:16 -0400

    Update QuotientException.java

--- exception/example1/QuotientException.java
+++ exception/example1/QuotientException.java
@@ -2,6 +2,8 @@ public class QuotientException {
   public static void main(String[] args) {
     
     java.util.Scanner input = new java.util.Scanner;
-    System.out.println("Enter numerator integer
+    System.out.println("Enter numerator integer:");
+    int numerator = input.nextInt();
+    
   }
 }
